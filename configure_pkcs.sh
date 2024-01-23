1e39e0482652185465b22fd4fdf1ca8b01e82fa2
Enrique Belarte Luque <ebelarte@redhat.com>
2024-01-23 10:12:59 -0600

    Add new version to AWS SDK docker image

--- configure_pkcs.sh
+++ configure_pkcs.sh
@@ -3,11 +3,8 @@
 sed  -i '1i openssl_conf = openssl_init' /etc/pki/tls/openssl.cnf 
 cat /etc/aws-kms-pkcs11/openssl-pkcs11.conf >> /etc/pki/tls/openssl.cnf
 # Set AWS KMS Token ID from variable
-echo "AWS_KMS_KEY_ID = $AWS_KMS_KEY_ID"
-export AWS_KMS_TOKEN=$(echo $AWS_KMS_KEY_ID | cut -c1-32)
-echo "AWS_KMS_TOKEN = $AWS_KMS_TOKEN"
 sed -i "s/MY_KMS_ID/$AWS_KMS_KEY_ID/g" /etc/aws-kms-pkcs11/config.json
 # Set the module path
 export PKCS11_MODULE_PATH=/usr/lib64/pkcs11/aws_kms_pkcs11.so
 # Create the public x509 certificate for later signing process
-openssl req -config /etc/aws-kms-pkcs11/x509.genkey -x509 -key "pkcs11:model=0;manufacturer=aws_kms;serial=0;token=$AWS_KMS_TOKEN" -keyform engine -engine pkcs11 -out /etc/aws-kms-pkcs11/mycert.pem -days 36500
+openssl req -config /etc/aws-kms-pkcs11/x509.genkey -x509 -key "pkcs11:model=0;manufacturer=aws_kms;serial=0;token=$KMS_TOKEN" -keyform engine -engine pkcs11 -out /etc/aws-kms-pkcs11/mycert.pem -days 36500
